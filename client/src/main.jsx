3d70ff509dde3fede1e706685dc5a11c94a2fae0
motty mandel <mandelmottyisrael65@gmail.com>
2023-11-30 20:32:15 -0500

    react skeleton

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import App from './App';
+import Home from './pages/Home';
+
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <div>404</div>,
+    children: [
+      {
+        index: true,
+        element: <Home/>
+      },
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
