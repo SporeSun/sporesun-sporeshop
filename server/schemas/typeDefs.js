31bbb0ff85ebb9ecb0d4a2c2c9a1f7609b5615b2
Devin Amlen <devinamlen@gmail.com>
2023-11-30 19:28:29 -0500

    outlining typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,24 +6,62 @@ const typeDefs = `
 
   }
 
+  type PrePurchase {
+    item: Item
+    product: Product
+    amount: Int
+  }
+
   type Product {
-    
+    _id: ID
+    parent: Item!
+    price: Float!
+    stock: Int!
+  }
+
+  type Item {
+    _id: ID
+    name: String!
+    description: String
+    image: String
   }
 
   type Cart {
-
+    _id: ID
+    contents: [PrePurchase]
   }
 
   type Tag {
-
+    _id: ID
+    tag: String!
   }
 
   type User {
-    
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    isAdmin: Boolean
+
+  }
+
+  type Store {
+    displayName: String
+    owner: User
+    transactionHistory: [Transaction]
+    categories: [Category]
+    stock: [Product]
   }
 
   type Transaction {
-
+    _id: ID!
+    date: String!
+    store: Store!
+    buyer: User!
+    seller: User!
+    items: Cart!
+    cost: Float!
+    status: String!
   }
 
   type Auth {
