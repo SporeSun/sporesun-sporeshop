4a47cad027eb29d91853f56c54cf096431eda61b
Devin Amlen <devinamlen@gmail.com>
2023-12-06 18:42:10 -0500

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,6 @@
 const User = require('./User');
 const Transaction = require('./Transaction')
-const Cart = require('./Cart')
 const Category = require('./Category')
 const PrePurchase = require('./PrePurchase')
 const Product = require('./Product')
-module.exports = { User, Transaction, Cart, Category, PrePurchase, Product };
+module.exports = { User, Transaction, Category, PrePurchase, Product };
