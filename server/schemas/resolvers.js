77826c9db8ea951f0afcec976a78440cb9894206
Devin Amlen <devinamlen@gmail.com>
2023-12-06 10:09:01 -0500

    updating user schema in parity with new name system

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -55,8 +55,8 @@ const resolvers = {
 
       return { token, user };
     },
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
+    addUser: async (parent, { firstName, lastName, email, password }) => {
+      const user = await User.create({ firstName, lastName, email, password });
       const token = signToken(user);
       return { token, user };
     },
