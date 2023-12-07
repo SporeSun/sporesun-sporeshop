e7c9f20d49ee7e422917923a458e130559d6196f
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-12-06 17:42:26 -0800

    solved merge issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
-export default function App() {
-    return (
-        <div>
-            <Header />
-              <Outlet />
-            <Footer />
-        </div>
-    );
+import Footer from "./components/Footer"
+import Home from "./pages/Home";
+import CartScreen from "./pages/CartScreen";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import ProductsPage from "./pages/ProductsPage";
+import CategoryScreen from "./pages/CategoryScreen";
+
+function App() {
+  return (
+    <>
+    <Header/>
+      <Routes>
+        <Route index={true} path="/" element={<Home />} />
+        <Route path="/cart" element={<CartScreen/>} />
+        <Route path="/products" element={<ProductsPage/>} />
+        <Route path="/category/:category" element={<CategoryScreen/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+       </Routes>
+      <Footer/>
+    </>
+  );
 }
+
+export default App;
