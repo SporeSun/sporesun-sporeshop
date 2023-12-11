9a135a85310be4304f244860f2fd50da8a3879e9
Devin Amlen <devinamlen@gmail.com>
2023-12-11 17:39:14 -0500

    Update resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -74,6 +74,11 @@ const resolvers = {
   },
 
   Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
